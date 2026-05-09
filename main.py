fd5077cbcb573ce8464c2cbaeb7dfabc0fcf52c0
alexdevzz <alexdev.workenv@gmail.com>
2026-05-08 21:36:49 -0400

    feat(main): adding setup logging and optimize imports
    fix(.gitignore): exclude log files

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import asyncio
+import os
 import sys
 
-from config.config import load_config
-
 # cargar la configuracion
+from config.config import load_config
 config = load_config()
+
+# Configurar logging
+from logger.logger import setup_logging
+setup_logging()
 
 # Añadir configuraciones extras a Crawlee antes de que se inicialice.
 # Configurar el directorio de almacenamiento de Crawlee (antes de importar otras cosas)
 from crawlee.configuration import Configuration
-
 Configuration.get_global_configuration().storage_dir = config['storage']['path']
 
 # importar modulos que usan Crawlee
